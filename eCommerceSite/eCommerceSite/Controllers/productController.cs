9102ba849016b4b3cce7980914ce2ff9cfecfad2
ShahriarShaikat <shahriarshaikat4@gmail.com>
2022-03-08 11:22:14 +0600

    Update

--- eCommerceSite/eCommerceSite/Controllers/productController.cs
+++ eCommerceSite/eCommerceSite/Controllers/productController.cs
@@ -24,6 +24,7 @@ namespace eCommerceSite.Controllers
             var pr = (from data in db.products
                      where data.id == id
                      select data).FirstOrDefault();
+            pr.qty = 1;
             if(Session["cart"] == null)
             {
                 List<product> productList = new List<product>();
@@ -35,10 +36,24 @@ namespace eCommerceSite.Controllers
             {
                 string json = Session["cart"].ToString();
                 var d = new JavaScriptSerializer().Deserialize<List<product>>(json);
-                d.Add(pr);
+                bool flag = true;
+                foreach(var datas in d)
+                {
+                    if(datas.id == pr.id)
+                    {
+                        datas.qty = datas.qty + 1;
+                        flag = false;
+                    }
+                }
+                if(flag)
+                {
+                    d.Add(pr);
+                }
                 var listP = new JavaScriptSerializer().Serialize(d);
                 Session["cart"] = listP.ToString();
             }
+            Session["msg"] = "Product added to the cart!";
+            //TempData["msg"] = "Temporay message";
             return RedirectToAction("ViewCart");
         }
 
@@ -60,15 +75,20 @@ namespace eCommerceSite.Controllers
         }
         public ActionResult DeleteProduct(int id)
         {
-            eCommerceEntities db = new eCommerceEntities();
+            /*eCommerceEntities db = new eCommerceEntities();
             var matchObj = (from data in db.products
                       where data.id == id
-                      select data).FirstOrDefault();
+                      select data).FirstOrDefault();*/
             string json = Session["cart"].ToString();
             var d = new JavaScriptSerializer().Deserialize<List<product>>(json);
+            var matchObj = (from data in d
+                            where data.id == id
+                            select data).FirstOrDefault();
+            //d.Remove(d.FirstOrDefault(e => e.id == matchObj.id));
             d.Remove(matchObj);
             var listP = new JavaScriptSerializer().Serialize(d);
             Session["cart"] = listP.ToString();
+            TempData["msg"] = "Product deleted!";
             return RedirectToAction("ViewCart");
         }
     }
